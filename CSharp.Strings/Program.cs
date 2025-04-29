6b9018707dc2082c53418e95b11775d2de69738a
Solmaz Purser <solmaz@hotmail.co.uk>
2025-04-29 16:14:28 +0100

    Strings: completed challenge

--- CSharp.Strings/Program.cs
+++ CSharp.Strings/Program.cs
@@ -1,18 +1,36 @@
 ﻿namespace CSharp.Strings;
-
-class Order {
-internal string total;
+// class Order {
+// internal string total;
     
-    internal Order(string total) {
-        this.total = total;
+//     internal Order(string total) {
+//         this.total = total;
+//     }
+
+//     public string FriendlyTotal() {
+//         return $"Hello there! The total is {total}";
+//     }
+//     static void Main(string[] args)
+//     {
+//         Order message = new Order("£50");
+//         Console.WriteLine(message.FriendlyTotal());
+//     }
+// }
+
+class StoryBuilder {
+    internal string plot;
+    internal StoryBuilder(string plot) {
+        this.plot = plot;
     }
 
-    public string FriendlyTotal() {
-        return $"Hello there! The total is {total}";
+    public string AddPlotline(string idea) {
+        return plot + idea;
     }
-    static void Main(string[] args)
-    {
-        Order message = new Order("£50");
-        Console.WriteLine(message.FriendlyTotal());
+    public string GetPlot() {
+        return plot;
+    }
+    static void Main(string[] args) {
+        StoryBuilder fairytale = new StoryBuilder("Once upon a time, ");
+        string snowWhite = fairytale.AddPlotline("there was a girl called Snow White");
+        Console.WriteLine(snowWhite);
     }
 }
