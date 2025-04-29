279f2177f8f894a57fbdaa1f127acaa11562312c
Solmaz Purser <solmazpurser@MASTUDENT-Solmaz-Purser-Y7526Y37KC.local>
2025-04-29 10:50:17 +0100

    c# work for makers

---
+++ CSharp.InstanceFields/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp.InstanceFields;
+
+class Post {
+// This class has two fields both hold strings
+string content;
+string author;
+// constructor
+  Post(string content, string author) {
+    this.content = content;
+    this.author = author;
+  }
+
+}
+Post weather = new Post("The weather today is great", "Serena");
+Post holiday = new Post("Just about to head off to the Solomon Islands!", "Kevwe");
---
+++ CSharp.InstanceMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp.InstanceMethods; //namespace groups related classes together
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Program myProgram = new Program(); //create an instance of program or object. myProgram can call instance methods like Hello().
+        string printOnly = myProgram.Hello(); //calls hello with no arguments
+        Console.WriteLine(printOnly); // prints hello only
+        string name = myProgram.Hello("Dave"); //calls hello with argument defined as name
+        Console.WriteLine(name); // prints name
+
+    }
+    string Hello()
+    {
+        return "Hello";
+    }
+
+    string Hello(string name)
+    {
+        return $"Hello, {name}";
+    }
+    
+}
