6ab68e94ed0f1266874125f7bcb935040460029b
Solmaz Purser <solmaz@hotmail.co.uk>
2025-04-29 16:06:01 +0100

    Strings: completed exercise

--- CSharp.Strings/Program.cs
+++ CSharp.Strings/Program.cs
@@ -1,9 +1,18 @@
 ﻿namespace CSharp.Strings;
 
-class Program
-{
+class Order {
+internal string total;
+    
+    internal Order(string total) {
+        this.total = total;
+    }
+
+    public string FriendlyTotal() {
+        return $"Hello there! The total is {total}";
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Order message = new Order("£50");
+        Console.WriteLine(message.FriendlyTotal());
     }
 }
